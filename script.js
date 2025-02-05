9c02bd7cfd2ca67148cb71ee1dcc382e1ee3f8ca
Villaflor, Justin John Mico <justinjohnmico.09@gmail.com>
2025-02-05 23:07:55 +0800

    UPDATE structure

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
         });
     });
 });
+
+const container = document.getElementById("line-container");
+
+// Generate multiple lines
+for (let i = 0; i < 15; i++) {
+  const line = document.createElement("div");
+  line.classList.add("line");
+  const xPosition = Math.random() * 100 + "vw";
+  line.style.setProperty("--x", xPosition);
+  line.style.animationDelay = Math.random() * -50 + "s"; /* Adjusted to match slower movement */
+  container.appendChild(line);
+}
