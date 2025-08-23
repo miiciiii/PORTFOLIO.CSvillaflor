e9366d30981aedbd268306e71b330c6c74252a1b
Villaflor, Justin John Mico <152823210+miiciiii@users.noreply.github.com>
2025-08-23 23:08:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -636,7 +636,7 @@ const milestones = [
     year: "2021 - Year Start", 
     title: "Start of Undergraduate Studies", 
     description: "Enrolled at Laguna State Polytechnic University, taking Bachelor of Science in Computer Science. Adjusted and thrived in a new environment.", 
-    image: "images/placeholders/2021-university.jpg" 
+    image: "images/placeholders/2024-freelance.png" 
   },
   { 
     year: "2021 - Year End", 
@@ -654,7 +654,7 @@ const milestones = [
     year: "2024", 
     title: "Freelance Web Development", 
     description: "Took on multiple web development projects, earning income while strengthening skills and helping finance academic needs.", 
-    image: "images/placeholders/2024-freelance.png" 
+    image: "images/placeholders/2021-university.jpg"
   },
   { 
     year: "2025", 
@@ -743,4 +743,5 @@ modal.addEventListener("click", (e) => {
   if (e.target === modal) {
     modal.style.display = "none";
   }
+
 });
